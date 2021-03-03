6b0e9a33085fe8a99924598e8e632d2a378943a2
Australopithecus91 <wild1991bear@gmail.com>
2021-03-03 21:10:47 +0300

    new function

--- ADC_Convert_2021/Core/Src/Usart_2_ini.c
+++ ADC_Convert_2021/Core/Src/Usart_2_ini.c
@@ -19,3 +19,13 @@ void usart_transmit(void) // функция для передачи данных
 	HAL_Delay(500); 
 	
 }
+
+
+void usart_transmitor(void) // функция для передачи данных  по usart
+{
+	//voltage = convert_adc();
+    sprintf(tran_str, "voltage %.2f", voltage);
+    HAL_UART_Transmit(&huart2, (uint8_t*)tran_str, strlen(tran_str), 1000);
+	HAL_Delay(500);
+
+}
